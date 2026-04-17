b55faa868e76424007a6cda30aa623b6a2088299
kchow39 <kachow@csumb.edu>
2026-04-16 20:36:23 -0700

    video 5

--- app/src/main/java/com/example/hw04_gymlog_v300/MainActivity.java
+++ app/src/main/java/com/example/hw04_gymlog_v300/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        repository = new GymLogRepository(getApplication());
+        repository = GymLogRepository.getRepository(getApplication());
 
         binding.logDisplayTextView.setMovementMethod(new ScrollingMovementMethod());
 
@@ -50,8 +50,8 @@ public class MainActivity extends AppCompatActivity {
         String currentInfo = binding.logDisplayTextView.getText().toString();
         Log.d(TAG, "current info: "+currentInfo);
         String newDisplay = String.format(Locale.US,"Exercise:%s%nWeight:%.2f%nReps:%d%n=-=-=-=%n%s",mExercise,mWeight,mReps,currentInfo);
-
         binding.logDisplayTextView.setText(newDisplay);
+        Log.i(TAG, repository.getAllLogs().toString());
     }
     private void getInformationFromDisplay(){
         mExercise = binding.exerciseInputEditText.getText().toString();
